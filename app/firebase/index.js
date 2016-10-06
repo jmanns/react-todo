31ff3ddae999ee9f9f96bc3f7d1a1c02dcc385cd
Jason Manns <Jason.Manns@gmx.com>
2016-10-06 20:39:45 +0100

    Move firebase creds to env vars

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,16 +2,16 @@ import firebase from 'firebase';
 
 try {
   const config = {
-    apiKey: "AIzaSyAjBKChuZNH5A1mQPvL_JXVlFK8WsP08Cg",
-    authDomain: "jmanns-todo-app.firebaseapp.com",
-    databaseURL: "https://jmanns-todo-app.firebaseio.com",
-    storageBucket: "jmanns-todo-app.appspot.com",
-    messagingSenderId: "844377557424"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    // messagingSenderId: "844377557424"
   };
 
   firebase.initializeApp(config);
 } catch (e) {
-  
+
 }
 
 export const firebaseRef =  firebase.database().ref();
